22509292b0ae288ae159d331b617bcdcc8671366
Tatsiana Marozava <mortatval@gmail.com>
2024-02-21 15:17:58 +0700

    seminar_4

--- app/src/main/java/com/mortatval/seminar_4/MainActivity.kt
+++ app/src/main/java/com/mortatval/seminar_4/MainActivity.kt
@@ -8,22 +8,18 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initActivityState()
     }
-
     private fun checkConditions(): Boolean {
         val conditionNameField = binding.username.length() in 1..40
         val conditionPhoneField = binding.phone.text.toString().isNotBlank() &&
                 binding.phone.toString().isNotEmpty()
         val conditionSexIsChecked = binding.male.isChecked ||
                 binding.female.isChecked
-
         val conditionNotificationSwitch = (binding.notifications.isChecked &&
                 binding.authorization.isChecked || binding.offers.isChecked)
                 || !binding.notifications.isChecked
@@ -31,7 +27,6 @@ class MainActivity : AppCompatActivity() {
         return conditionSexIsChecked && conditionNameField
                 && conditionPhoneField && conditionNotificationSwitch
     }
-
     private fun changeSaveButtonState() {
         if (checkConditions()) {
             binding.saveButton.isEnabled = true
@@ -39,16 +34,11 @@ class MainActivity : AppCompatActivity() {
             binding.saveButton.isEnabled = false
         }
     }
-
-
     private fun initActivityState() {
         val numberOfPoints = Random.nextInt(101)
-
         binding.progressBar.secondaryProgress = numberOfPoints
-
         binding.PointsOfNumber.text =
             String.format(getString(R.string.points_of_number, numberOfPoints))
-
         binding.offers.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 changeSaveButtonState()
@@ -56,7 +46,6 @@ class MainActivity : AppCompatActivity() {
                 changeSaveButtonState()
             }
         }
-
         binding.authorization.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 changeSaveButtonState()
@@ -64,16 +53,12 @@ class MainActivity : AppCompatActivity() {
                 changeSaveButtonState()
             }
         }
-
         binding.username.doAfterTextChanged {
             changeSaveButtonState()
         }
-
         binding.phone.doAfterTextChanged {
             changeSaveButtonState()
         }
-
-
         binding.notifications.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 binding.authorization.isEnabled = true
@@ -87,17 +72,13 @@ class MainActivity : AppCompatActivity() {
                 changeSaveButtonState()
             }
         }
-
-
         binding.gender.setOnCheckedChangeListener { _, _ ->
             changeSaveButtonState()
         }
-
         binding.saveButton.setOnClickListener {
             Toast.makeText(this, R.string.changes_saved, Toast.LENGTH_SHORT).show()
         }
     }
-
 }
 
 
